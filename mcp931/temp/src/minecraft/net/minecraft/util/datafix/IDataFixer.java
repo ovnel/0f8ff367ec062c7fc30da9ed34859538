d9f8695fd4a9ea88953a0d75ccea16a40630c675
inv41idu53rn4m3 <malli.onkki@gmail.com>
2016-06-25 10:11:18 +0300

    Removed some MCP files and emptied the MCP temp folder

--- mcp931/temp/src/minecraft/net/minecraft/util/datafix/IDataFixer.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.util.datafix;
-
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.datafix.IFixType;
-
-public interface IDataFixer {
-   NBTTagCompound func_188251_a(IFixType var1, NBTTagCompound var2, int var3);
-}
--- mcp931/temp/src/minecraft/net/minecraft/util/datafix/IDataWalker.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.util.datafix;
-
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.datafix.IDataFixer;
-
-public interface IDataWalker {
-   NBTTagCompound func_188266_a(IDataFixer var1, NBTTagCompound var2, int var3);
-}
