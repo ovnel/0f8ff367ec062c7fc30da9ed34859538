e5ca13814e596cc78af7631095ba07e57b33763e
inv41idu53rn4m3 <malli.onkki@gmail.com>
2016-06-25 16:20:48 +0300

    More work on the infra. It's going really slow.

---
+++ mcp931/src/minecraft/vort3x/CommandManager.java
@@ -0,0 +1,12 @@
+package vort3x;
+
+import java.util.ArrayList;
+
+public class CommandManager{
+	
+	/* Handles all the client side commands
+	 * and parses chat entries */
+	
+	// Used for iterating through all the commands
+	private static ArrayList<Command> commands = new ArrayList<Command>();
+}
